f1de44d3ed1e8c058247f42bc7f8b28b719c4199
hunge20 <eh629@cornell.edu>
2022-04-06 23:17:58 -0400

    Created login screen

---
+++ CrimeMap/CrimeMap/LoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginViewController.swift
+//  CrimeMap
+//
+//  Created by Elysia Hung on 4/6/22.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+    
+    
+    @IBAction func onSignUp(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CrimeMap/Pods/Target Support Files/Pods-CrimeMap-CrimeMapUITests/Pods-CrimeMap-CrimeMapUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CrimeMap_CrimeMapUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CrimeMap_CrimeMapUITests
+@end
