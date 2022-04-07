988514e72292e9d2178122bbe2156f198daa706d
hunge20 <eh629@cornell.edu>
2022-04-07 18:48:33 -0400

    Completed login screen and function

--- CrimeMap/CrimeMap/LoginViewController.swift
+++ CrimeMap/CrimeMap/LoginViewController.swift
@@ -24,10 +24,31 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil{
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else{
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
     }
     
     
     @IBAction func onSignUp(_ sender: Any) {
+        var user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text;
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else{
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
     }
     
     
