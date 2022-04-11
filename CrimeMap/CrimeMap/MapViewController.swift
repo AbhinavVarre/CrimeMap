19cb21ee673bfa1204db0ec2b74381e2395b9333
AbhinavVarre <37716743+AbhinavVarre@users.noreply.github.com>
2022-04-11 18:41:50 -0500

    added annotations and user location
    I put in a sample annotation for the map, but still have to do an actual custom view to see the details of the crime. Also have to implement reading from our database to autogenerate annotations.

--- CrimeMap/CrimeMap/MapViewController.swift
+++ CrimeMap/CrimeMap/MapViewController.swift
@@ -6,13 +6,50 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let location = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        location.desiredAccuracy = kCLLocationAccuracyBest
+        location.delegate = self
+        location.requestWhenInUseAuthorization()
+        location.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first{
+            manager.stopUpdatingLocation()
+            
+            renderLocation(location: location)
+        }
+    }
+    
+    func renderLocation( location: CLLocation) {
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+        mapView.showsUserLocation = true;
+        //Working on the annotationsa and getting a detail page up and running
+        let sampleAnnotationPin = MKPointAnnotation()
+        sampleAnnotationPin.coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude + 0.001, longitude: location.coordinate.longitude + 0.001)
+        sampleAnnotationPin.title = "Mugging"
+        mapView.addAnnotation(sampleAnnotationPin)
+        
     }
     
 
