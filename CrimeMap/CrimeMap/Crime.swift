668af9e949db5c13b62c9cc9a73773841c064388
AbhinavVarre <37716743+AbhinavVarre@users.noreply.github.com>
2022-04-22 23:32:05 -0500

    adding crime file

---
+++ CrimeMap/CrimeMap/Crime.swift
@@ -0,0 +1,20 @@
+//
+//  Crime.swift
+//  CrimeMap
+//
+//  Created by Abhinav Varre on 4/18/22.
+//
+
+import Foundation
+import CoreLocation
+
+class Crime {
+    
+    let title: String = ""
+    let description: String = ""
+    let location: CLLocation
+    
+
+    
+     
+}
